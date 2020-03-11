f005d8da69fe62561edc88012ae7cc3a98b0af20
Francisco Morais <35690067+franciscomorais@users.noreply.github.com>
2020-03-11 17:49:55 +0000

    Add project files

---
+++ index.js
@@ -0,0 +1,91 @@
+
+/**
+ * Module dependencies.
+ */
+
+import {
+  NativeModules,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
+
+import Draggable from 'react-native-draggable';
+import React from 'react';
+
+/**
+ * Styles.
+ */
+
+const styles = {
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'green',
+    borderColor: 'white',
+    borderRadius: 150,
+    borderWidth: 1,
+    height: 44,
+    justifyContent: 'center',
+    opacity: 0.4,
+    width: 44
+  },
+  label: {
+    color: '#ffffff',
+    fontSize: 12
+  }
+};
+
+/**
+ * Handle debug menu.
+ */
+
+const handleDebugMenu = () => {
+  NativeModules.DevMenu.show();
+};
+
+/**
+ * `FloatDevMenu` component.
+ */
+
+const FloatDevMenu = props => {
+  if (!__DEV__) {
+    return props.children;
+  }
+
+  const { children, name, startX, startY } = props;
+
+  return (
+    <>
+      {children}
+
+      <Draggable
+        x={startX}
+        y={startY}
+      >
+        <TouchableWithoutFeedback onPress={handleDebugMenu}>
+          <View style={styles.button}>
+            <Text style={styles.label}>
+              {name}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </Draggable>
+    </>
+  );
+};
+
+/**
+ * Default props.
+ */
+
+FloatDevMenu.defaultProps = {
+  name: 'DEV',
+  startX: 50,
+  startY: 50
+};
+
+/**
+ * Export `FloatDevMenu`.
+ */
+
+export default FloatDevMenu;
